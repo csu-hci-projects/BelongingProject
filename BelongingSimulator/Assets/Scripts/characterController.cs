f17aced5606765fd25f6fc95ff639b3f4ceba74d
Vidya Gaddy <gaddvi@rams.colostate.edu>
2019-10-25 15:36:58 -0600

    add unity project and intro

---
+++ BelongingSimulator/Assets/Scripts/characterController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+}
